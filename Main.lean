647fa77a3a4abfac852913f093cf65d74ea08bf0
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2026-06-25 20:14:47 +0800

    init with nightly

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Velvet2
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ Velvet2.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Velvet2` library.
+-- Import modules here that should be built as part of the library.
+import Velvet2.Basic
